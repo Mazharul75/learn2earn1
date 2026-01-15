7de993d6bd0bf2adb8fa767d134550f75b63358b
Mazharul75 <mazharulislam.nabil321@gmail.com>
2026-01-16 00:21:28 +0600

    database name fixed

--- config/config.php
+++ config/config.php
@@ -3,10 +3,10 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'learn2earn');
+define('DB_NAME', 'learn2earn1');
 
 // Base URL
-define('BASE_URL', 'http://localhost/learn2earn/public');
+define('BASE_URL', 'http://localhost/learn2earn1/public');
 
 // Start session
 session_start();
