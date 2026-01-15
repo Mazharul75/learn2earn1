2d721683b6462e61447c49501d1276e0b8b4ffc6
Mazharul75 <mazharulislam.nabil321@gmail.com>
2026-01-16 00:17:31 +0600

    database connection

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'learn2earn');
+
+// Base URL
+define('BASE_URL', 'http://localhost/learn2earn/public');
+
+// Start session
+session_start();
