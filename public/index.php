ae2b77bca800df0f5d44383f0593aab75c048997
Niloy-Bhuiyan <niloybhuiyann@gmail.com>
2026-01-16 01:14:43 +0600

    testing To find out error reason

--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
 require_once "../config/config.php";
+require_once "../app/core/App.php";
+require_once "../app/core/Controller.php";
+require_once "../app/core/Model.php";
 require_once "../app/core/Database.php";
 
-$db = new Database();
-echo "Database Connected Successfully";
+$app = new App();
 ?>
