f57fa5d66000f10f92aad277a6e75fba40d0c097
Mazharul75 <mazharulislam.nabil321@gmail.com>
2026-01-17 05:38:23 +0600

    Add course management view and material upload UI

---
+++ app/views/instructor/materials.php
@@ -0,0 +1,7 @@
+<h2>Upload Materials for Course</h2>
+<form action="<?= BASE_URL ?>instructor/uploadMaterial" method="POST">
+    <input type="hidden" name="course_id" value="<?= $data['course_id'] ?>">
+    <input type="text" name="title" placeholder="Material Title (e.g. Lecture 1)" required><br><br>
+    <input type="text" name="file_path" placeholder="URL or File Name" required><br><br>
+    <button type="submit">Add Material</button>
+</form>
