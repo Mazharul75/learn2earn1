6195cd6db2aa6ff5df15da2d223e3e2723199561
Niloy-Bhuiyan <niloybhuiyann@gmail.com>
2026-01-20 03:56:16 +0600

    config reviwed

--- config/config.php
+++ config/config.php
@@ -4,8 +4,8 @@ define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_NAME', 'learn2earn');
 
-// The Query String Routing URL [cite: 1816]
+
 define('BASE_URL', 'http://localhost/learn2earn/');
 
-// CRITICAL: This must be at the very top of the config
+
 session_start();
